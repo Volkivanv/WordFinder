2be2ac2bc147e455e32ac3b24664bc2b7eaa3332
Ivan Volkov <volkivanv@mail.ru>
2021-09-15 00:06:20 +0300

    Second Commit

--- main.cpp
+++ main.cpp
@@ -4,22 +4,26 @@
 
 int main() {
     std::ifstream notebook;
-
-    std::string wordNeed;
-    std::cout<<"Input please word for finding!"<< std::endl;
-    std::getline(std::cin,wordNeed);
     notebook.open("Notebook.txt");
-    int count = 0;
-    while (!notebook.eof()){
-        std::string wordFind;
-        notebook >> wordFind;
-        if(wordNeed == wordFind){
-            count++;
+    if(notebook) {
+        std::string wordNeed;
+        std::cout << "Input please word for finding!" << std::endl;
+        std::getline(std::cin, wordNeed);
+
+        int count = 0;
+        while (!notebook.eof()) {
+            std::string wordFind;
+            notebook >> wordFind;
+            if (wordNeed == wordFind) {
+                count++;
+            }
         }
+
+
+        std::cout << "The word " << wordNeed << " appears in the text " << count << " times." << std::endl;
+        notebook.close();
+    } else {
+        std::cerr << "File is not found!"<< std::endl;
     }
-
-
-    std::cout << "The word "<< wordNeed <<" appears in the text " << count << " times." << std::endl;
-    notebook.close();
     return 0;
 }
